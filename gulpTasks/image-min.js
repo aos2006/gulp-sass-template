9d7de3adf437dfb272193d2f156140146a2bb80d
anton <vashchenko-01@mail.ru>
2016-11-08 13:35:29 +0300

    fixes

--- gulpTasks/image-min.js
+++ gulpTasks/image-min.js
@@ -1,3 +1,15 @@
-/**
- * Created by anton on 08.11.16.
- */
+const gulp = require('gulp'),
+    l = require('gulp-load-plugins')(),
+    combine = require('stream-combiner2').obj,
+    rev = require('gulp-rev'),
+    imagemin = require('gulp-imagemin');
+
+module.exports = function(options){
+  return function () {
+    return combine(
+        gulp.src(options.src),
+            imagemin(),
+            gulp.dest(options.dist)
+    )
+  }
+};
--- gulpTasks/image-resize.js
+++ gulpTasks/image-resize.js
@@ -1,3 +1,20 @@
-/**
- * Created by anton on 08.11.16.
- */
+const gulp = require('gulp'),
+    l = require('gulp-load-plugins')(),
+    combine = require('stream-combiner2').obj,
+    gulpSharp = require('gulp-sharp');
+
+module.exports = function(options){
+  return function () {
+
+    return combine(
+        gulp.src(options.src),
+            gulpSharp({
+              resize : [1920, 995],
+              max : true,
+              quality : 80,
+              progressive : true
+            }),
+            gulp.dest(options.dist)
+    );
+  }
+};
